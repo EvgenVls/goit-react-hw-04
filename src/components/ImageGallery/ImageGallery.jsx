c83d7808d9b662c3419d34116c6bc0cd14d3aeb0
Yevgen Vlasenko <evgenvl@ukr.net>
2024-04-22 20:51:28 +0300

    update gallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,9 +4,11 @@ export default function ImageGallery({ items }) {
   return (
     <ul>
       {items.map((item) => {
-        <li key={item.id}>
-          <ImageCard url={item.urls} descr={item.description} />
-        </li>;
+        return (
+          <li key={item.id}>
+            <ImageCard url={item.urls} descr={item.description} />
+          </li>
+        );
       })}
     </ul>
   );
